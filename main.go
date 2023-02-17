af2065ddd03726155ce0e5c766165ac22a1059dd
remy <remysalem@hotmail.fr>
2023-02-17 22:40:15 +0100

    remove get db from main

--- main.go
+++ main.go
@@ -2,16 +2,11 @@ package main
 
 import (
 	api "jump-backend-interview/src/api"
-	db "jump-backend-interview/src/database"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	println("Starting server...");
-	err := db.GetDb().Ping();
-	if err != nil {
-		panic(err)
-	}
 	router := gin.New()
 	router.SetTrustedProxies(nil)
 	api.InitApi(router)
