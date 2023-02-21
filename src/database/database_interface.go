f1f4ff6ff8550fe06a398cf68fb4421efe4e84b8
remy <remysalem@hotmail.fr>
2023-02-21 12:09:23 +0100

    simplify sql query and add error handing on PostInvoice

--- src/database/database_interface.go
+++ src/database/database_interface.go
@@ -2,13 +2,14 @@ package database
 
 import (
 	model "jump-backend-interview/src/model"
+	"log"
 )
 
 func GetUsers() (*[]model.User, int) {
 	db := GetDb()
-	rows, res := db.Query("SELECT * FROM users")
-	if res != nil {
-		println(res.Error())
+	rows, err := db.Query("SELECT * FROM users")
+	if err != nil {
+		log.Default().Println(err.Error())
 		return nil, 500
 	}
 	defer rows.Close()
@@ -19,7 +20,7 @@ func GetUsers() (*[]model.User, int) {
 		err := rows.Scan(&user.Id, &user.First_name, &user.Last_name, &balance)
 		user.Balance = float64(balance) / 100
 		if err != nil {
-			println(res.Error())
+			log.Default().Println(err.Error())
 			return nil, 500
 		}
 		users = append(users, user)
@@ -29,15 +30,22 @@ func GetUsers() (*[]model.User, int) {
 
 func PostInvoice(invoice model.Invoice) int {
 	db := GetDb()
+	var result bool
+
+	db.QueryRow("SELECT NOT EXISTS(SELECT 1 FROM users WHERE id = $1)", invoice.User_id).Scan(&result)
+	if result {
+		return 404
+	}
+
 	stmt, err := db.Prepare("INSERT INTO invoices (user_id, amount, label) VALUES ($1, $2, $3)")
 	if err != nil {
-		println(err.Error())
+		log.Default().Println(err.Error())
 		return 500
 	}
 	defer stmt.Close()
 	_, err = stmt.Exec(invoice.User_id, (invoice.Amount * 100), invoice.Label)
 	if err != nil {
-		println(err.Error())
+		log.Default().Println(err.Error())
 		return 500
 	}
 	return 204
@@ -48,19 +56,16 @@ func PostTransaction(transaction model.Transaction) int {
 	var result bool
 
 	db.QueryRow("SELECT NOT EXISTS(SELECT 1 FROM invoices WHERE id = $1)", transaction.Invoice_id).Scan(&result)
-	println("result 404: ", result)
 	if result {
 		return 404
 	}
 
 	db.QueryRow("SELECT EXISTS(SELECT 1 FROM invoices WHERE status = 'paid' AND id = $1)", transaction.Invoice_id).Scan(&result)
-	println("result 422: ", result)
 	if result {
 		return 422
 	}
 
 	db.QueryRow("SELECT NOT EXISTS(SELECT 1 FROM invoices WHERE id = $1 AND amount = $2)", transaction.Invoice_id, transaction.Amount*100).Scan(&result)
-	println("result 400: ", result)
 	if result {
 		return 400
 	}
@@ -75,26 +80,17 @@ func PostTransaction(transaction model.Transaction) int {
 
 	_, err := db.Exec(query, transaction.Invoice_id, (transaction.Amount * 100))
 	if err != nil {
-		println(err.Error())
+		log.Default().Println(err.Error())
 		return 500
 	}
 
-	query = `WITH invoice_to_pay AS (
-		SELECT i.id, i.status, i.amount, u.id AS user_id, u.balance
-		FROM invoices i
-		JOIN users u ON i.user_id = u.id
-		WHERE i.id = $1
-	)
+	query = `
 	UPDATE invoices i
-	SET status = s.status
-	FROM invoice_to_pay itp
-	JOIN invoice_status s ON s.status = 'paid'
-	WHERE i.id = itp.id
-	  AND itp.status = 'pending'
-	  AND itp.amount = $2`
-	_, err = db.Exec(query, transaction.Invoice_id, (transaction.Amount * 100))
+	SET status = 'paid'
+	WHERE i.id = $1`
+	_, err = db.Exec(query, transaction.Invoice_id)
 	if err != nil {
-		println(err.Error())
+		log.Default().Println(err.Error())
 		return 500
 	}
 	return 204
